fd7780990e087990f753147b84f57c0c3bbf79d5
viktaur <victorgabal@gmail.com>
2022-08-18 17:58:26 +0200

    basic home setup

---
+++ config.ts
@@ -0,0 +1,13 @@
+const configs = {
+    discord_invite: "https://discord.gg/3efMw45QMN",
+    linkedin: "https://www.linkedin.com/company/dsoc",
+    twitter: "https://twitter.com/SotonDsoc",
+    github: "https://github.com/soton-dsoc",
+    api: {
+        // baseUrl: "http://10.14.200.68:1337"
+        // baseUrl: "http://10.14.140.195:1337"
+        baseUrl: "http://localhost:1337"
+    }
+}
+
+export default configs;
