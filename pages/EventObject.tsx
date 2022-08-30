b6b885e9d28b62e1003b03dc04748c85087216cf
viktaur <victorgabal@gmail.com>
2022-08-30 13:28:56 +0200

    fix bug for firefox, change event colours

--- pages/EventObject.tsx
+++ pages/EventObject.tsx
@@ -3,7 +3,10 @@ import styles from '../styles/EventObject.module.css';
 function EventObject(props: any) {
     return(
         // if the event is upcoming, display it with its assigned colour
-        <div className={styles.eventBox} style={{ background: `${props.category === "upcoming" ? props.data.colour : ""}`}}>
+        // <div className={styles.eventBox} style={{ background: `${props.category === "upcoming" ? props.data.colour : ""}`}}>
+        
+        // using the blue-pink gradient might be better to stick to the button colours
+        <div className={styles.eventBox} style={{ background: `${props.category === "upcoming" ? "linear-gradient(90deg, #9000A8, #0066FF)" : ""}`}}>
             <div className={styles.title}>{props.data.title}</div>
             <div>{props.data.date.toUTCString()}</div>
             <div className={styles.description}>{props.data.description}</div>
