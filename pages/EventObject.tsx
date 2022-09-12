f3dc7fd82aea0577198e22528097778741a1c2a1
viktaur <victorgabal@gmail.com>
2022-09-13 00:45:17 +0200

    add animation

--- pages/EventObject.tsx
+++ pages/EventObject.tsx
@@ -14,7 +14,12 @@ function EventObject(props: any) {
     const organisers: string[] = eventData ? eventData.organisers : [];
     const speakers: string[] = eventData ? eventData.speakers : [];
     const learningOutcomes: string[] = eventData ? eventData.learningOutcomes : [];
-    const media = eventData ? eventData.media : [];
+    const media = eventData ? eventData.media : []
+
+    function getHeight() {
+        const elem = document.getElementById(styles.collapWrap);
+        return elem?.clientHeight
+    }
 
     return(
         <div className={styles.wrapper}>
@@ -22,58 +27,60 @@ function EventObject(props: any) {
                 <div className={styles.title}>{title}</div>
                 <div className={styles.date}>{date}</div>
             </div>
-            <div className={`${styles.collapsible} ${active ? styles.active : ""}`}>
-                <div className={styles.left}>
-                    <div className={styles.abstract}>
-                        <div className={styles.title}>Abstract</div>
-                        {
-                            abstract.map((s: string, i: number) =>
-                                <p key={i}>{s}</p>
-                            )
-                        }
+            <div className={`${styles.collapsible} ${active ? styles.active : ""}`} style={{ maxHeight: active ? getHeight() : "0px"}}>
+                <div className={styles.collapWrap} id={styles.collapWrap}>
+                    <div className={styles.left}>
+                        <div className={styles.abstract}>
+                            <div className={styles.title}>Abstract</div>
+                            {
+                                abstract.map((s: string, i: number) =>
+                                    <p key={i}>{s}</p>
+                                )
+                            }
+                        </div>
+                        <div className={styles.media}>
+                            {
+                                media.map((m: string, i: number) => 
+                                    <div className={styles.imageContainer} key={i}>
+                                        <Image className={styles.image} src={m} style={{ borderRadius: "10px"}} alt=""></Image>
+                                    </div>
+                                )
+                            }
+                        </div>
+                        <div style={{ display: `${props.category === "past" ? "block" : "none"}`}}>
+                            <p>This event has passed</p>
+                        </div>
                     </div>
-                    <div className={styles.media}>
-                        {
-                            media.map((m: string, i: number) => 
-                                <div className={styles.imageContainer} key={i}>
-                                    <Image className={styles.image} src={m} style={{ borderRadius: "10px"}} alt=""></Image>
-                                </div>
-                            )
-                        }
-                    </div>
-                    <div style={{ display: `${props.category === "past" ? "block" : "none"}`}}>
-                        <p>This event has passed</p>
-                    </div>
-                </div>
-                <div className={styles.right}>
-                    <div className={styles.dateAndLocation}>
-                        <div className={styles.title}>Date and Location</div>
-                        <p>{date}</p>
-                        <p>{location}</p>
-                    </div>
-                    <div className={styles.organisers}>
-                        <div className={styles.title}>Organisers</div>
-                        {
-                            organisers.map((organiser: string, i: number) =>
-                                <p key={i}>{organiser}</p>
-                            )
-                        }  
-                    </div>
-                    <div className={styles.speakers}>
-                        <div className={styles.title}>Speakers</div>
-                        {
-                            speakers.map((speaker: string, i: number) => 
-                                <p key={i}>{speaker}</p>
-                            )
-                        }
-                    </div>
-                    <div className={styles.learningOutcomes}>
-                        <div className={styles.title}>Learning Outcomes</div>
-                        {
-                            learningOutcomes.map((outcome: string, i: number) => 
-                                <p key={i}>{outcome}</p>
-                            )
-                        }
+                    <div className={styles.right}>
+                        <div className={styles.dateAndLocation}>
+                            <div className={styles.title}>Date and Location</div>
+                            <p>{date}</p>
+                            <p>{location}</p>
+                        </div>
+                        <div className={styles.organisers}>
+                            <div className={styles.title}>Organisers</div>
+                            {
+                                organisers.map((organiser: string, i: number) =>
+                                    <p key={i}>{organiser}</p>
+                                )
+                            }  
+                        </div>
+                        <div className={styles.speakers}>
+                            <div className={styles.title}>Speakers</div>
+                            {
+                                speakers.map((speaker: string, i: number) => 
+                                    <p key={i}>{speaker}</p>
+                                )
+                            }
+                        </div>
+                        <div className={styles.learningOutcomes}>
+                            <div className={styles.title}>Learning Outcomes</div>
+                            {
+                                learningOutcomes.map((outcome: string, i: number) => 
+                                    <p key={i}>{outcome}</p>
+                                )
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
